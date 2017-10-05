6ac243f75b52c4e4ee47be207df6cf3eabc96672
Ana Letícia <alcvlara@sga.pucminas.br>
2017-10-04 21:54:33 -0300

    Adding the post

--- blogs2b/blog/views.py
+++ blogs2b/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'index.html', {'posts': posts})
